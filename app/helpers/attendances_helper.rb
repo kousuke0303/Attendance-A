27c7b2487e87741a009d3aa9c5a2c2e1e975efc4
kousuke <kousukeee0303@gmail.com>
2019-10-10 02:33:48 +0000

    fin time

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -11,6 +11,14 @@ module AttendancesHelper
     format("%.2f", (((finish - start) / 60 ) / 60.0 ))
   end
   
+  # 時間外時間を計算
+  
   def overtime(day)
+    d_w_e_t_to_min = @user.designated_work_end_time.hour * 60 + @user.designated_work_end_time.min
+    if day.overtime_tomorrow_check == 0
+      format("%.2f", (day.plans_end_work_time.hour * 60 + day.plans_end_work_time.min - d_w_e_t_to_min) / 60.0)
+    elsif day.overtime_tomorrow_check == 1
+      format("%.2f", (1440 - d_w_e_t_to_min + day.plans_end_work_time.hour * 60 + day.plans_end_work_time.min) / 60.0)
+    end
   end
 end
